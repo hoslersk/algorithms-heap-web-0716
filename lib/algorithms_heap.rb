ee9d543f27c14628f8ad5ec79eb76b195d552fda
Schyler Hosler <schylerhosler@gmail.com>
2016-10-23 23:30:17 -0400

    Ruby practice with algorithms heap concept -- all tests passing

--- lib/algorithms_heap.rb
+++ lib/algorithms_heap.rb
@@ -1,1 +1,31 @@
 # code your solution here
+class MinHeap
+
+  attr_reader :data
+  attr_accessor :datum, :root
+
+  def initialize
+    @data = []
+    @root = nil
+  end
+
+  def push(datum)
+    if !self.root
+      self.root = datum
+    else
+      if self.root > datum
+        self.root = datum
+      end
+    end
+    self.data.push(datum)
+  end
+
+  def pop
+    if self.data.last == self.root
+      sorted_arr = self.data.sort
+      self.root = sorted_arr[1]
+    end
+    self.data.pop
+  end
+
+end
